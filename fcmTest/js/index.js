457166abe786ede0f6b1205a251573bd61933426
terry_lee <terry_lee@taogo.com.tw>
2018-03-28 17:22:01 +0800

    fcm

--- fcmTest/js/index.js
+++ fcmTest/js/index.js
@@ -1,16 +1,5 @@
 (function () {
 
-    // Initialize Firebase
-    var config = {
-        apiKey: 'AIzaSyB5c3U7oZdJZeIUD1rfKckvaI0gJW-gppc',
-        authDomain: 'tgo-test.firebaseapp.com',
-        databaseURL: 'https://tgo-test.firebaseio.com',
-        projectId: 'tgo-test',
-        storageBucket: '',
-        messagingSenderId: '572768582484'
-    };
-    firebase.initializeApp(config);
-
     // [START get_messaging_object]
     // Retrieve Firebase Messaging object.
     const messaging = firebase.messaging();
---
+++ fcmTest/js/init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: 'AIzaSyB5c3U7oZdJZeIUD1rfKckvaI0gJW-gppc',
+    authDomain: 'tgo-test.firebaseapp.com',
+    databaseURL: 'https://tgo-test.firebaseio.com',
+    projectId: 'tgo-test',
+    storageBucket: '',
+    messagingSenderId: '572768582484'
+};
+firebase.initializeApp(config);
