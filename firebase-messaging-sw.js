60e565f36be4a5485843e9b23c213f72442d17a4
terry_lee <terry_lee@taogo.com.tw>
2018-03-28 17:26:38 +0800

    fcm

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,9 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('./fcmTest/js/init.js');
 importScripts('https://www.gstatic.com/firebasejs/4.12.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.12.0/firebase-messaging.js');
+importScripts('./fcmTest/js/init.js');
 
 var messaging = firebase.messaging();
 
