a0861b234c5f6f32a3b9f14d2bf7cabad8fef71b
terry_lee <terry_lee@taogo.com.tw>
2018-08-21 15:07:10 +0800

    Update "scroll" and "detect scroll" file.

--- nativeScrollTo/scroll.js
+++ nativeScrollTo/scroll.js
@@ -5,20 +5,32 @@
         var scrollX = window.scrollX ? 'scrollX' : 'pageXOffset',
             scrollY = window.scrollY ? 'scrollY' : 'pageYOffset',
             SPEED = 15,
-    
+
             _x = el ? el.offsetLeft : 0,
             _y = el ? el.offsetTop : 0;
 
         function goScroll () {
 
             var currentX = window[scrollX],
-                currentY = window[scrollY];
+                currentY = window[scrollY],
+                checkX = currentX > _x,
+                checkY = currentY > _y;
 
-            if (currentY > _y) {
+            if (checkX || checkY) {
 
                 setTimeout(function () {
 
-                    currentY -= SPEED;
+                    if (checkX) {
+
+                        currentX -= SPEED;
+
+                    }
+
+                    if (checkY) {
+
+                        currentY -= SPEED;
+
+                    }
                     window.scrollTo(currentX, currentY);
                     goScroll();
 
