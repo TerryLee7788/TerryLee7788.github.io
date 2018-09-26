174e175a9d119f12f275809b532e5e6342c78a0b
TerryLee <k9114658@gmail.com>
2018-09-26 08:04:46 +0800

    Update "webpack.config.js" file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,35 @@
-var path = require('path');
+const path = require('path'),
+    MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: [],
-  output: {},
-  module: {
-    loades: [
-      {
-
-      }
-    ]
-  }
+    entry: {
+        index: path.resolve(__dirname, './js/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
+    ],
+    mode: 'development',
+    watch: true,
 };
